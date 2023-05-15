6da62401cb3d3aa5943f07f916e664483d128e63
TT3301 <16677718+TT3301@users.noreply.github.com>
2023-05-15 13:48:21 +0800

    Update IP.js

--- general/IP.js
+++ general/IP.js
@@ -5,8 +5,9 @@ if ($response.statusCode != 200) {
 var body = $response.body;
 var obj = JSON.parse(body);
 var title = obj['country'];
-var subtitle = obj['city'] + ' ' + obj['query'];
-var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "IP" + ":" + obj['query'] + '\n' + "运营商" + ":" + obj['isp'] + '\n' + "数据中心" + ":" + obj['org'];
+var subtitle = obj['city'] + ' ' + obj['isp'];
+var ip = obj['query'];
+var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "运营商" + ":" + obj['isp'] + '\n' + "数据中心" + ":" + obj['org'];
 
 
-$done({title, subtitle, description});
+$done({title, subtitle, ip, description});
