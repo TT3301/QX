5177def9419e8dbfce8cf70b4af5d953b7ca7487
TT3301 <16677718+TT3301@users.noreply.github.com>
2023-05-15 13:12:37 +0800

    Update IP.js

--- general/IP.js
+++ general/IP.js
@@ -4,7 +4,7 @@ if ($response.statusCode != 200) {
 
 var body = $response.body;
 var obj = JSON.parse(body);
-var title = obj['country'] + ' ' + obj['region'] + ' ' + obj['city'];
+var title = obj['country'] + ' ' + obj['city'];
 var subtitle = obj['org'] + ' ' + obj['query'];
 var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "IP" + ":" + obj['query'] + '\n' + "运营商" + ":" + obj['isp'] + '\n' + "数据中心" + ":" + obj['org'];
 
