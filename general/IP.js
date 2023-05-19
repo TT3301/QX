bcf441ff00ca7e461132fb76a79811a1b635ed73
TT3301 <16677718+TT3301@users.noreply.github.com>
2023-05-19 12:14:04 +0800

    Update IP.js

--- general/IP.js
+++ general/IP.js
@@ -42,7 +42,7 @@ var flags = new Map([["AC","🇦🇨"],["AD","🇦🇩"],["AE","🇦🇪"],["AF"
 
 var body = $response.body;
 var obj = JSON.parse(body);
-var title =flags.get(obj['countryCode']) + ' '+ City_ValidCheck(obj['city'])+ ' ' +Area_check(obj['country']);
+var title =flags.get(obj['countryCode']) + ' '+ City_ValidCheck(obj['city'])+ ' ' + Area_check(obj['country']);
 var subtitle = ISP_ValidCheck(obj['org']);
 var ip = obj['query'];
 var description = '服务商:'+obj['isp'] + '\n'+'地区:' +City_ValidCheck(obj['regionName'])+ '\n' + 'IP:'+ obj['query'] + '\n' +'时区:'+ obj['timezone'];
