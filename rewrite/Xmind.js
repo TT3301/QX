72a5893ab92d5438619c8be46cbf0a3933cbc5fa
TT3301 <16677718+TT3301@users.noreply.github.com>
2024-01-29 11:00:04 +0800

    Update Xmind.js

--- rewrite/Xmind.js
+++ rewrite/Xmind.js
@@ -1,6 +1,6 @@
 /*************************************
 
-项目名称：Xmind-思维导图 解锁VIP
+项目名称：Xmind-思维导图
 下载地址：https://t.cn/AipCL5zE
 脚本作者：chxm1023
 电报频道：https://t.me/chxm1023
@@ -10,23 +10,31 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/www\.xmind\.(cn|net|app)\/.+\/devices url script-response-body https://raw.githubusercontent.com/TT3301/QX/main/rewrite/Xmind.js
+^https?:\/\/(?:www\.)?xmind\..*\/.+\/(devices|token\/.+) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Xmind.js
 
 [mitm]
-hostname = www.xmind.*
+hostname = *xmind.*
 
 *************************************/
 
 
 var chxm1023 = JSON.parse($response.body);
+const vip = /https:\/\/www\.xmind\.cn\/_res\/devices/;
+const token = /https:\/\/xmind\.cn\/_res\/token\/.+/;
 
-chxm1023 = {
-  "license" : {
-    "status" : "sub",
-    "expireTime" : 4092599349000
-  },
-  "_code" : 200,
-  "raw_data" : "bKnTeMMu/TJ86ZFoUqG+sfKC1nitD8b/NaUT93UBc0woo0BF5uBVYXsHUQJkQxLHGlZCbdnKaKzZ4iljM9xddkyMycpUbAtDoywnr4RNzxqXY20pDM0Bdgm+HssaS1RworH8aaaf0IYnnGplxIuqMyT9107PrenF0MBUIaKU9Cw="
-};
+if (vip.test($request.url)) {
+    chxm1023 = {
+      "license" : {
+        "status" : "sub",
+        "expireTime" : 4092599349000
+      },
+      "_code" : 200,
+      "raw_data" : "GfxYgAqnrQ\/ggD9UwqnZyAj6FKnopXzM8s5m3eZLOsmVr4FwCzv41qTmgi\/u72cv+jpaAoljaEPti1twzOS7X7KUPY1KNJ2xalbS7SXbvFHSvs21QXjaUtIOkeJWAl4\/vHl4IvMeHTBVqD8mFCXOmvnBPLRMAJEPgHEJYF1InvQ="};
+}
+
+if (token.test($request.url)) {
+    chxm1023.expireDate = 4092599349;
+    chxm1023.token = "f50633ea8eb04cbb85962b99c47045d7AjOobEGo";
+}
 
 $done({body : JSON.stringify(chxm1023)});
